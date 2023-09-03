a46c4f2cd25cc0f0d1bcf895104b6985c87c7f51
Jonatan Lifshits <djonli2003@gmail.com>
2023-09-03 18:06:29 +0300

    Testing autoResetEvent

---
+++ AutoResetEvent_test/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int x = 0;
+
+AutoResetEvent autoResetEvent = new AutoResetEvent(true);
+
+for (int i = 0; i < 5; i++) 
+{
+    Thread t = new (Print);
+    t.Name = $"Thread {i}";
+    t.Start();
+}
+
+
+void Print()
+{
+    autoResetEvent.WaitOne();
+    x = 1;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+        ++x;
+        Thread.Sleep(500);
+    }
+    autoResetEvent.Set();
+}
